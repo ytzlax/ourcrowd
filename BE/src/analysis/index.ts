7d4dea9b4885f8a28368cc52c72c5b98bc49bfe5
ytzlax <ytzlax@gmail.com>
2026-07-29 11:18:32 +0300

    feat(analysis): create module

--- BE/src/analysis/index.ts
+++ BE/src/analysis/index.ts
@@ -1,1 +1,10 @@
-// Analysis: normalize raw mentions and define LLM sentiment prompts
+export { SentimentType } from "./sentiment_type.js";
+export { SentimentAnalyzer } from "./sentiment_analyzer.js";
+export { normalizeMentionForAnalysis } from "./mention_normalizer.js";
+export {
+  SENTIMENT_DECISION_SCHEMA,
+  type AnalyzedMention,
+  type RawSentimentDecision,
+} from "./analysis_types.js";
+export type { SentimentAnalyzerConfig } from "./sentiment_analyzer.js";
+export type { NormalizedMentionContent } from "./mention_normalizer.js";
