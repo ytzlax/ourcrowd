413a181bea4ac4edfc9ec83dab499de9bc6073fe
ytzlax <ytzlax@gmail.com>
2026-07-29 10:18:48 +0300

    feat(llm): provider router

--- BE/src/llm/index.ts
+++ BE/src/llm/index.ts
@@ -1,4 +1,5 @@
 export { Llm } from "./llm.js";
+export { LlmRouter } from "./llm_router.js";
 export { DEFAULT_LLM_MODEL, LlmModel } from "./llm_model.js";
 export type {
   JsonSchema,
@@ -9,3 +10,7 @@ export type {
   OllamaGenerateRequest,
   OllamaGenerateResponse,
 } from "./types.js";
+export type {
+  CompanyMetadata,
+  RouteDecision,
+} from "./router_types.js";
